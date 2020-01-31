62888bf731e5aaf7fa4fd9b26d7fd028141b950a
Beardocracy <travisjbearden@gmail.com>
2020-01-31 23:34:26 +0000

    version with range for iterations

--- factors.py
+++ factors.py
@@ -2,28 +2,15 @@
 
 import sys
 
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-
 with open(sys.argv[1], 'r') as f:
     numbers = [int(num) for num in f.read().split()]
-    
-    for num in numbers:
-        flag = 0
-        print("{}=".format(num), end='')
-        for prime in primes:
-            if num % prime == 0:
-                print("{}*{}".format(prime, num // prime))
-                flag = 1
-                break
-        if flag == 0:
-            for divisors in range(29, num // 2, 2):
-                if num % divisors == 0:
-                    print("{}*{}".format(divisors, num // divisors))
-                    break
-    
-    '''
-    line = f.readlines()
-    for element in line:
-        element = element.strip('\n') 
-    print(line)
-    '''
+    length = len(numbers)
+    for i in range(0, length):
+            num = numbers[i]
+            if num % 2 == 0:
+                print("{}={}*{}".format(num, 2, num // 2))
+            else:
+                for divisors in range(3, num // 2, 2):
+                    if num % divisors == 0:
+                        print("{}={}*{}".format(num, divisors, num // divisors))
+                        break
