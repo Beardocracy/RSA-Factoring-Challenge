77af9710b7fa92ac5a25e7e58aedae2fc33e9008
Beardocracy <travisjbearden@gmail.com>
2020-01-31 23:38:32 +0000

    Stopped creating a list

--- factors.py
+++ factors.py
@@ -3,14 +3,12 @@
 import sys
 
 with open(sys.argv[1], 'r') as f:
-    numbers = [int(num) for num in f.read().split()]
-    length = len(numbers)
-    for i in range(0, length):
-            num = numbers[i]
-            if num % 2 == 0:
-                print("{}={}*{}".format(num, 2, num // 2))
-            else:
-                for divisors in range(3, num // 2, 2):
-                    if num % divisors == 0:
-                        print("{}={}*{}".format(num, divisors, num // divisors))
-                        break
+    for num in f.read().split():
+        num = int(num)
+        if num % 2 == 0:
+            print("{}={}*{}".format(num, 2, num // 2))
+        else: 
+            for divisors in range(3, num // 2, 2):
+                if num % divisors == 0:
+                    print("{}={}*{}".format(num, divisors, num // divisors))
+                    break
